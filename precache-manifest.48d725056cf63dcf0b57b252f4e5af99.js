dd473cc69a093d777fec863064f3e10470b8123d
Masa <masaal808@gmail.com>
2023-06-20 04:27:25 -0700

    Updates

---
+++ precache-manifest.48d725056cf63dcf0b57b252f4e5af99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "75bfd4ffe0a60fd9a503",
+    "url": "/HierarchicalToDo/static/css/main.e7079946.chunk.css"
+  },
+  {
+    "revision": "75bfd4ffe0a60fd9a503",
+    "url": "/HierarchicalToDo/static/js/main.75bfd4ff.chunk.js"
+  },
+  {
+    "revision": "4b01829277567b620b6b",
+    "url": "/HierarchicalToDo/static/js/1.4b018292.chunk.js"
+  },
+  {
+    "revision": "328e582731503034d839",
+    "url": "/HierarchicalToDo/static/js/runtime~main.328e5827.js"
+  },
+  {
+    "revision": "99747275531d3b466f56ddda6b5c0b62",
+    "url": "/HierarchicalToDo/index.html"
+  }
+];
